91f841659f9198950f8f423ab12b6b4ad28a7a7d
Vladimir Steshin <warheim24@gmail.com>
2022-03-24 13:01:11 +0300

    Dict function added, Listed files function corrected

--- files_hw.py
+++ files_hw.py
@@ -1,9 +1,18 @@
 import os
 
-file_path = os.path.join(os.getcwd(), 'For merge')
+file_path_merge = os.path.join(os.getcwd(), 'For merge')
 
-with os.scandir(file_path) as objects:
+with os.scandir(file_path_merge) as objects:
     file_list = []
     for file in objects:
-        if 'txt' in file.name and 'Re' not in file.name:
+        if 'txt' in file.name:
             file_list.append(file.name)
+
+
+def file_sort(library):
+    file_dict = {}
+    for doc in library:
+        with open(doc, encoding='UTF-8') as content:
+            file_dict[doc] = content.read().strip()
+    return file_dict
+
