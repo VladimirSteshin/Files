6ffd0f71c158967f8502f8588a8996c0fccf4e63
Vladimir Steshin <warheim24@gmail.com>
2022-03-24 12:57:32 +0300

    Listed files

--- files_hw.py
+++ files_hw.py
@@ -1,3 +1,9 @@
 import os
 
 file_path = os.path.join(os.getcwd(), 'For merge')
+
+with os.scandir(file_path) as objects:
+    file_list = []
+    for file in objects:
+        if 'txt' in file.name and 'Re' not in file.name:
+            file_list.append(file.name)
